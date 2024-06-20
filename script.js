d7182fe574ddd9ac6983bf0045b2d30504acb84a
Ryan <r803425@gmail.com>
2024-06-20 20:53:55 +0800

    Complete persistence

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+let listState = [];
+
+const STATE_KEY = "todo-list";
+
+function loadState() {
+  const listState = localStorage.getItem(STATE_KEY);
+  if (listState !== null) {
+    return JSON.parse(listState);
+  }
+  return [];
+}
+
+function saveState(list) {
+  localStorage.setItem(STATE_KEY, JSON.stringify(list));
+}
+
+function initList() {
+  // load state
+  listState = loadState();
+  // render list
+  const ul = document.getElementById("list");
+  for (const item of listState) {
+    const li = document.createElement("li");
+    li.innerText = item.text;
+
+    const deleteButton = document.createElement("span");
+    deleteButton.classList.add("delete");
+    deleteButton.onclick = deleteItem;
+    li.appendChild(deleteButton);
+
+    li.classList.add("item");
+    if (item.checked) {
+      li.classList.add("checked");
+    }
+
+    li.onclick = checkItem;
+
+    ul.appendChild(li);
+  }
+}
+
 function addItem() {
   const ul = document.getElementById("list");
   const input = document.getElementById("input");
@@ -19,20 +60,38 @@ function addItem() {
 
   newItem.appendChild(deleteButton);
 
+  listState.push({
+    text,
+    checked: false,
+  });
+  saveState(listState);
+
   input.value = "";
   ul.appendChild(newItem);
 }
 
 function checkItem() {
   const item = this;
+  const parent = item.parentNode;
+  const idx = Array.from(parent.childNodes).indexOf(item);
+
+  listState[idx].checked = !listState[idx].checked;
+
   item.classList.toggle("checked");
+
+  saveState(listState);
 }
 
 function deleteItem() {
   const item = this.parentNode;
   const parent = item.parentNode;
+  const idx = Array.from(parent.childNodes).indexOf(item);
+  listState = listState.filter((_, i) => i !== idx);
   parent.removeChild(item);
+  saveState(listState);
 }
+
+initList();
 
 const addButton = document.getElementById("add-button");
 addButton.addEventListener("click", addItem);
@@ -41,3 +100,13 @@ const form = document.getElementById("input-wrapper");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
+
+// const deleteButtons = document.querySelectorAll(".delete");
+// for (const button of deleteButtons) {
+//   button.onclick = deleteItem;
+// }
+
+// const listItems = document.querySelectorAll(".item");
+// for (const item of listItems) {
+//   item.onclick = checkItem;
+// }
